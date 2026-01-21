WillsB3/np

commit e996c6f5c5508305b8869ed53f03b3c9250775b6

Fix subfolder publishing broken since npm 8.5
npm 8.5+ has a bug where `npm publish <folder>` publishes from the current working directory instead of from the specified folder. This causes the published package to incorrectly include files from both the root directory and the subfolder.

Fixes #656

source/index.js
@@ -196,11 +196,11 @@ const np = async (input = 'patch', {packageManager, ...options}, {package_, root
 					task(context, task) {
 						let hasError = false;
 
-						return from(runPublish(getPublishCommand(options)))
+						return from(runPublish(getPublishCommand(options), {cwd: options.rootDirectory}))
 							.pipe(catchError(error => handleNpmError(error, task, otp => {
 								context.otp = otp;
 
-								return runPublish(getPublishCommand({...options, otp}));
+								return runPublish(getPublishCommand({...options, otp}), {cwd: options.rootDirectory});
 							})))
 							.pipe(
 								// Note: Cannot use `async` here as the `await` will not finish before the error propagates.

source/npm/publish.js
@@ -1,17 +1,7 @@
-import path from 'node:path';
 import {execa} from 'execa';
 
 export const getPackagePublishArguments = options => {
 	const arguments_ = ['publish'];
-
-	if (options.contents) {
-		// Normalize to explicit relative path so npm doesn't interpret it as a package name
-		const contents = path.isAbsolute(options.contents) || options.contents.startsWith('.')
-			? options.contents
-			: `./${options.contents}`;
-
-		arguments_.push(contents);
-	}
 
 	if (options.tag) {
 		arguments_.push('--tag', options.tag);
@@ -28,8 +18,17 @@ export const getPackagePublishArguments = options => {
 	return arguments_;
 };
 
-export function runPublish(arguments_) {
-	const cp = execa(...arguments_);
+export function runPublish(arguments_, options = {}) {
+	const execaOptions = {};
+
+	// `npm` 8.5+ has a bug where `npm publish <folder>` publishes from cwd instead of <folder>.
+	// We work around this by changing cwd to the target directory.
+	// https://github.com/npm/cli/issues/5136
+	if (options.cwd) {
+		execaOptions.cwd = options.cwd;
+	}
+
+	const cp = execa(...arguments_, execaOptions);
 
 	cp.stdout.on('data', chunk => {
 		// https://github.com/yarnpkg/berry/blob/a3e5695186f2aec3a68810acafc6c9b1e45191da/packages/plugin-npm/sources/npmHttpUtils.ts#L541

test/npm/publish.js
@@ -1,38 +1,10 @@
 import test from 'ava';
-import {getPackagePublishArguments} from '../../source/npm/publish.js';
+import {getPackagePublishArguments, runPublish} from '../../source/npm/publish.js';
 
 test('no options set', t => {
 	t.deepEqual(
 		getPackagePublishArguments({}),
 		['publish'],
-	);
-});
-
-test('options.contents - normalizes relative path', t => {
-	t.deepEqual(
-		getPackagePublishArguments({contents: 'dist'}),
-		['publish', './dist'],
-	);
-});
-
-test('options.contents - preserves ./ prefix', t => {
-	t.deepEqual(
-		getPackagePublishArguments({contents: './dist'}),
-		['publish', './dist'],
-	);
-});
-
-test('options.contents - preserves ../ prefix', t => {
-	t.deepEqual(
-		getPackagePublishArguments({contents: '../dist'}),
-		['publish', '../dist'],
-	);
-});
-
-test('options.contents - preserves absolute path', t => {
-	t.deepEqual(
-		getPackagePublishArguments({contents: '/absolute/path'}),
-		['publish', '/absolute/path'],
 	);
 });
 
@@ -55,4 +27,9 @@ test('options.publishScoped', t => {
 		getPackagePublishArguments({publishScoped: true}),
 		['publish', '--access', 'public'],
 	);
+});
+
+test('runPublish uses cwd option when provided', async t => {
+	const result = await runPublish(['echo', ['test']], {cwd: '/tmp'});
+	t.is(result.cwd, '/tmp');
 });

commit b5d9fee11dfe95ebe0659082813fd811df0694af

Fix: Propagate rootDirectory to options for publish task
This fixes the bug where npm publish was running from the wrong directory
when using --contents flag. The rootDirectory was being calculated but not
added to the options object that gets passed to tasks.

Fixes issue introduced in e996c6f.

source/index.js
@@ -59,8 +59,8 @@ const np = async (input = 'patch', {packageManager, ...options}, {package_, root
 	}
 
 	// Add rootDirectory to options so it's available in tasks
-	// TEMPORARILY COMMENTED OUT TO TEST LOGGING
-	// options.rootDirectory = rootDirectory;
+	// This fixes a bug introduced in e996c6f where rootDirectory wasn't propagated
+	options.rootDirectory = rootDirectory;
 
 	const runTests = options.tests && !options.yolo;
 	const runCleanup = options.cleanup && !options.yolo;